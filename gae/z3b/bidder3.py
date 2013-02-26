ffbc8f86a1715bc3a55ed8874f6441ecdab47018
Eric Seidel <github@eseidel.com>
2013-02-26 02:25:28 -0800

    Make the new z3 interpreter actually compile and do something.

--- gae/z3b/bidder3.py
+++ gae/z3b/bidder3.py
@@ -1,5 +1,8 @@
 from z3 import *
 from third_party import enum
+from core.callhistory import CallHistory
+from core.call import Call
+
 
 spades, hearts, diamonds, clubs, points = Ints('spades hearts diamonds clubs points')
 
@@ -35,7 +38,25 @@ rule_of_fifteen = spades + points >= 15
 # Tie-breaker-priorites -- planner stage, when 2 bids match which we make.
 
 class Rule(object):
-    pass
+    preconditions = []
+    category = None
+
+    def make_call(self, knowlege):
+        # FIXME: It's unclear if this should take a call_history or not
+        # in the kbb, the call history is not passed to apply_rule.
+        for precondition in self.preconditions:
+            if not precondition.matches(knowledge):
+                return None
+        # FIXME: kbb rules can make more than one type of call, it's unclear
+        # if we should follow that model here or not.
+        return Call(self.call_name)
+
+    def possible_priorities_and_conditions_for_call(self, call):
+        # We're eventually going to allow more than one call per rule.
+        assert call.name == self.call_name, self.call_name
+        for condition, priority in self.conditional_priorites:
+            yield priority, condition
+        yield self.priority, True
 
 
 opening_priorities = enum.Enum(
@@ -49,28 +70,32 @@ opening_priorities = enum.Enum(
 
 # FIXME: This is preconditioned on no-one having opened.
 class OneClubOpening(Rule):
-    constraints = [rule_of_twenty, clubs >= 3]
+    call_name = '1C'
+    constraints = And(rule_of_twenty, clubs >= 3)
     conditional_priorites = [
         (Or(clubs > diamonds, clubs == diamonds == 3), opening_priorities.LongestMinor),
     ]
     priority = opening_priorities.LowerMinor
 
 class OneDiamondOpening(Rule):
-    constraints = [rule_of_twenty, diamonds >= 3]
+    call_name = '1D'
+    constraints = And(rule_of_twenty, diamonds >= 3)
     conditional_priorites = [
         (diamonds > clubs, opening_priorities.LongestMinor),
     ]
     priority = opening_priorities.HigherMinor
 
 class OneHeartOpening(Rule):
-    constraints = [rule_of_twenty, hearts >= 5]
+    call_name = '1H'
+    constraints = And(rule_of_twenty, hearts >= 5)
     conditional_priorites = [
         (hearts > spades, opening_priorities.LongestMajor),
     ]
     priority = opening_priorities.LowerMajor
 
 class OneSpadeOpening(Rule):
-    constraints = [rule_of_twenty, spades >= 5]
+    call_name = '1S'
+    constraints = And(rule_of_twenty, spades >= 5)
     conditional_priorites = [
         (spades > hearts, opening_priorities.LongestMajor),
     ]
@@ -88,11 +113,11 @@ response_priorites = enum.Enum(
 )
 
 class OneDiamondResponse:
-    constraints = [points >= 6, diamonds >= 4]
+    constraints = And(points >= 6, diamonds >= 4)
     priority = response_priorites.OneDiamondResponse
 
 class OneHeartResponse:
-    constraints = [points >= 6, hearts >= 4]
+    constraints = And(points >= 6, hearts >= 4)
     conditional_priorites = [
         (And(hearts >= 5, hearts > spades), response_priorites.LongestNewMajor),
         (hearts >= 5, response_priorites.OneHeartWithFiveResponse),
@@ -100,31 +125,24 @@ class OneHeartResponse:
     priority = response_priorites.OneHeartWithFourResponse
 
 class OneSpadeResponse:
-    constraints = [points >= 6, spades >= 4]
+    constraints = And(points >= 6, spades >= 4)
     conditional_priorites = [
         (spades >= 5, response_priorites.OneSpadeWithFiveResponse)
     ]
     priority = response_priorites.OneSpadeWithFourResponse
 
 class OneNotrumpResponse:
-    constraints = [points >= 6]
+    constraints = points >= 6
     priority = response_priorites.OneNotrumpResponse
 
-
-class PositionKnowledge(object):
-    def __init__(self):
-        self._exprs = []
-
-    def add(exprs):
-        self._exprs.extend(exprs)
 
 
 class Knowledge(object):
     def __init__(self):
-        self.me = PositionKnowledge()
-        self.rho = PositionKnowledge()
-        self.partner = PositionKnowledge()
-        self.lho = PositionKnowledge()
+        self.me = []
+        self.rho = []
+        self.partner = []
+        self.lho = []
 
     def rotate(self):
         old_me = self.me
@@ -133,8 +151,58 @@ class Knowledge(object):
         self.partner = self.rho
         self.rho = old_me
 
+
 class Bidder(object):
     pass
+
+
+class RuleSelector(object):
+    def __init__(self, rules, call_history, knowledge):
+        self._rules = rules
+        self.call_history = call_history
+        self.knowledge = knowledge
+        self._call_to_rule_cache = None
+        self._call_to_compiled_constraints = {}
+
+    def _call_to_rule_map(self):
+        if self._call_to_rule_cache is not None:
+            return self._call_to_rule_cache
+
+        self._call_to_rule_cache = {}
+        for rule in self._rules:
+            call = rule.make_call(self.knowledge)
+            if call:
+                exisiting_rule = self._call_to_rule_cache.get(call)
+                if not exisiting_rule or rule.category > exisiting_rule.category:
+                    self._call_to_rule_cache[call] = rule
+        return self._call_to_rule_cache
+
+    # FIXME: This could just use @memoized?
+    def rule_for_call(self, call_to_lookup):
+        return self._call_to_rule_map()[call_to_lookup]
+
+    # FIXME: This could just use @memoized?
+    def compile_constraints_for_call(self, call_to_lookup):
+        constraints = self._call_to_compiled_constraints.get(call_to_lookup)
+        if constraints:
+            return constraints
+
+        # (Or(clubs > diamonds, clubs == diamonds == 3) AND !(ROT AND hearts >= 5) AND !(ROT AND spades >= 5))
+        # OR
+        # (!Or(clubs > diamonds, clubs == diamonds == 3) AND !(ROT AND diamonds >=3) AND !(ROT AND hearts >= 5) AND !(ROT AND spades >= 5))
+
+        situations = []
+        used_rule = self.rule_for_call(call_to_lookup)
+        for priority, condition in used_rule.possible_priorities_and_conditions_for_call(call_to_lookup):
+            situational_constraints = [condition]
+            for unmade_call, unmade_rule in self._call_to_rule_map().iteritems():
+                for unmade_priority, unmade_condition in unmade_rule.possible_priorities_and_conditions_for_call(unmade_call):
+                    if unmade_priority > priority:
+                        situational_constraints.append(Not(And(unmade_condition, unmade_rule.constraints)))
+            situations.append(And(situational_constraints))
+        constraints = Or(situations)
+        self._call_to_compiled_constraints[call_to_lookup] = constraints
+        return constraints
 
 
 class Interpreter(object):
@@ -154,7 +222,6 @@ class Interpreter(object):
         # return a list of priority_conditions + constraints
         # FIXME: Isn't SuitedToPlay always going to contribute many available bids?
         # We should probably only consult rules if they can produce bids of higher priority.
-        rules = [rule for rule in self.rules if rule.matches_preconditions(call_history, knowledge)]
         bid_to_priority_and_constraints
         for rule in rules:
             rule.add_constraints_for_all_conditions_above_priority(priority, )
@@ -170,24 +237,24 @@ class Interpreter(object):
         knowledge = Knowledge()
         viewer = call_history.position_to_call()
 
+        # Generate bid -> rules map from history/knowledge
+        # filter to bid -> rule map
+        # compile to bid -> knowledge-list map
+        # validity check of knowledge
+
         for partial_history in call_history.ascending_partial_histories(step=1):
+            selector = RuleSelector(self.rules, partial_history, knowledge)
+
             call = partial_history.last_call()
-            caller = partial_history.last_to_call()
-            position_knowledge = knowlege.me
+            position_knowledge = knowledge.me
             partial_history_before_last_call = partial_history.copy_with_partial_history(-1)
 
-            # First get all the rules who have something to say about this call history.
-            # This checks pre-conditions, as well as for the presence of this particular call.
-            rules = [rule for rule in self.rules if rule.could_generate_last_call(call_history, knowledge)]
-            # Select the one with the highest intra-bid priority.  There should be exactly one.
-            rule = self._select_highest_priority_rule(rules)
+            rule = selector.rule_for_call(call)
+            # We can interpret bids we know how to make.
+            if rule:
+                # Now we know which rule they must have used to make this bid, we need to know which priorities it could have been.
+                knowledge.me.append(selector.compile_constraints_for_call(call))
 
-            # Now we know which rule they must have used to make this bid, we need to know which priorities it could have been.
-            # FIXME: We could simplify the list of priorities down to the lowest non-comparible priorities.
-            for priority in rule.possible_priorities_for_call(partial_history):
-                for constraints in self._constraints_for_calls_with_priorites_greater_than(priority, partial_history_before_last_call, knowledge):
-                    # They didn't make this bid, so !(condition AND constraints) --> add to knowledge
-                    position_knowledge.add(Not(constraints))
             knowledge.rotate()
 
         return knowledge
@@ -207,3 +274,6 @@ solver = Solver()
 solver.add(axioms)
 
 print solver.check()
+
+interpreter = Interpreter()
+print interpreter.knowledge_from_history(CallHistory.from_string('1H')).rho
