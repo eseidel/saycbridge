0382f777a1a94affd95d459c1e0960dae5db58d9
Eric Seidel <github@eseidel.com>
2013-08-10 19:03:08 -0700

    Add a better production server configuration

--- dist/gae/standalone_main.py
+++ dist/gae/standalone_main.py
@@ -1,16 +1,4 @@
-import werkzeug
 import werkzeug.serving
+import standalone_app
 
-import appengine_main
-import os.path
-
-gae_dir = os.path.dirname(__file__)
-
-app = werkzeug.SharedDataMiddleware(appengine_main.app, {
-    '/scripts': os.path.join(gae_dir, 'scripts'),
-    '/stylesheets': os.path.join(gae_dir, 'stylesheets'),
-    '/images': os.path.join(gae_dir, 'images'),
-    '/static': os.path.join(gae_dir, 'static'),
-})
-
-werkzeug.serving.run_simple('localhost', 8080, app, use_reloader=True)
+werkzeug.serving.run_simple('localhost', 8080, standalone_app.app, use_reloader=True)
