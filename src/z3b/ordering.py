3c89caa402bfa972fcf54c7cdb28afd1e08c78f3
Andrew Bortz <andrew@abortz.net>
2013-08-14 20:49:20 -0700

    New implementation of Ordering with networkx

--- src/z3b/ordering.py
+++ src/z3b/ordering.py
@@ -1,4 +1,5 @@
 import functools
+import networkx
 
 class Ordering(object):
     @functools.total_ordering
@@ -14,55 +15,46 @@ class Ordering(object):
             return self._ordering.lt(self._item, other._item)
 
     def __init__(self):
-        self._items = set()
-        self._mapping = {}
+        self._graph = networkx.DiGraph()
         self._compiled = True
 
     def lt(self, left, right):
         self._compile()
 
-        return bool(self._mapping.get((left, right)))
-
-    def cmp(self, left, right):
-        self._compile()
-
-        if self._mapping.get((left, right)):
-            return -1
-        elif self._mapping.get((right, left)):
-            return 1
-        else:
-            return 0
+        return self._graph.has_edge(left, right)
 
     def key(self, item):
         return Ordering.OrderedItem(self, item)
 
     def order(self, *args):
         self._compiled = False
-        for i in range(len(args)-1):
-            self._order(args[i], args[i+1])
 
         result = set()
         for blob in args:
             for item in self._iterate(blob):
                 result.add(item)
+
+        for i in range(len(args)-1):
+            for lower in self._iterate(args[i]):
+                for higher in self._iterate(args[i+1]):
+                    self._graph.add_edge(lower, higher)
+
         return result
 
     def _compile(self):
         if self._compiled:
             return
 
-        for middle in self._items:
-            for lower in self._items:
-                for higher in self._items:
-                    if self._mapping.get((lower, middle)) and self._mapping.get((middle, higher)):
-                        self._mapping[(lower, higher)] = True
+        for left in self._graph.nodes_iter():
+            for right in networkx.descendants(self._graph, left):
+                self._graph.add_edge(left, right)
 
         self._check_cycles()
         self._compiled = True
 
     def _check_cycles(self):
-        for item in self._items:
-            assert not self._mapping.get((item, item)), "Cycle detected for item %s" % repr(item)
+        if not networkx.is_directed_acyclic_graph(self._graph):
+            assert False, "Cycle detected"
 
     def _iterate(self, list_or_not):
         try:
@@ -70,13 +62,3 @@ class Ordering(object):
                 yield item
         except TypeError:
             yield list_or_not
-
-    def _order(self, lower_list, higher_list):
-        for lower in self._iterate(lower_list):
-            self._items.add(lower)
-        for higher in self._iterate(higher_list):
-            self._items.add(higher)
-
-        for lower in self._iterate(lower_list):
-            for higher in self._iterate(higher_list):
-                self._mapping[(lower, higher)] = True
