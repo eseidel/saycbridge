decfe925fa0e2269c8b9c8dfcdc155130fdacb29
Eric Seidel <github@eseidel.com>
2013-08-26 16:24:24 -0500

    Teach CallExplorer how to produce histories from a glob string.

--- src/core/callexplorer.py
+++ src/core/callexplorer.py
@@ -40,3 +40,42 @@ class CallExplorer(object):
             future_history.calls.append(call)
             yield future_history
             future_history.calls.pop()
+
+    def _split_before_last_token(self, string, delimeter=" "):
+        last_delimiter_index = string.rfind(delimeter)
+        if last_delimiter_index == -1:
+            return "", string
+        return string[:last_delimiter_index], string[last_delimiter_index + 1:]
+
+    def _has_wildcards(self, string):
+        return "*" in string
+
+    def _match_pattern_over(self, history, pattern):
+        # FIXME: We could support fancier pattern matching.
+        assert pattern == "*"
+        for call in self.possible_calls_over(history):
+            yield call
+
+    def _glob_helper(self, history, call_name):
+        call = Call.from_string(call_name)
+        # call_name can be empty if the original string is emtpy or there is trailing whitespace.
+        if call and history.is_legal_call(call):
+            yield call
+
+    # FIXME: Unclear if history_iglob should be so tolerant.
+    def _normalize_glob_string(self, glob_string):
+        # Leading/trailing whitespace will confuse our algorithm.
+        return glob_string.replace(",", " ").replace("  ", " ").strip()
+
+    def history_iglob(self, glob_string):
+        glob_string = self._normalize_glob_string(glob_string)
+        glob_string, last_token = self._split_before_last_token(glob_string)
+        histories = self.history_iglob(glob_string) if self._has_wildcards(glob_string) else [CallHistory.from_string(glob_string)]
+        call_generator = self._match_pattern_over if self._has_wildcards(last_token) else self._glob_helper
+
+        for history in histories:
+            for call in call_generator(history, last_token):
+                yield history.copy_appending_call(call)
+
+    def history_glob(self, glob_string):
+        return list(self.history_iglob(glob_string))
---
+++ src/core/tests/test_callexplorer.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2013 The SAYCBridge Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import unittest2
+from core.callexplorer import *
+
+
+class CallExplorerTest(unittest2.TestCase):
+    def _assert_histories(self, glob_string, histories):
+    	explorer = CallExplorer()
+        self.assertEqual(sorted(map(lambda history: history.calls_string(), explorer.history_glob(glob_string))), sorted(histories))
+
+    def test_history_glob(self):
+        self._assert_histories("", [])
+        self._assert_histories(" ", [])
+        self._assert_histories("P", ["P"])
+        self._assert_histories(" P ", ["P"])
+        self._assert_histories("P  1C", ["P 1C"])
+        self._assert_histories("* 1C", ["P 1C"])
+        self._assert_histories("1C * 1H", ["1C 1D 1H", "1C X 1H", "1C P 1H"])
+
+
+if __name__ == '__main__':
+    unittest2.main()
