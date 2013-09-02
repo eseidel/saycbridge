af4c24dc7ac5b05ea509b5b6d95d22395aa2d409
Eric Seidel <github@eseidel.com>
2013-09-02 11:47:23 -0700

    Make the dev server advertise on your local ip as well as localhost (for easier testing on mobile)

--- dist/gae/standalone_main.py
+++ dist/gae/standalone_main.py
@@ -1,4 +1,4 @@
 import werkzeug.serving
 import standalone_app
 
-werkzeug.serving.run_simple('localhost', 8080, standalone_app.app, use_reloader=True)
+werkzeug.serving.run_simple('0.0.0.0', 8080, standalone_app.app, use_reloader=True)
