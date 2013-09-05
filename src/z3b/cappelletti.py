2b6a1b83bec10c34ffaf140cb0644b9d43d0fde0
Eric Seidel <github@eseidel.com>
2013-09-04 22:54:50 -0700

    Make Cappelletti possible with only 10hcp

--- src/z3b/cappelletti.py
+++ src/z3b/cappelletti.py
@@ -40,7 +40,7 @@ class Cappelletti(Rule):
     # The book suggests "decent strength".
     # The book bids Cappelletti with 11 hcp, but seems to want 12 hcp when responding.
     # Wikipedia says Cappelletti is 9-14 hcp.
-    shared_constraints = points >= 11
+    shared_constraints = points >= 10
 
 
 rule_order.order(
@@ -99,22 +99,6 @@ cappelletti_two_diamonds_invitational_responses = set([
 rule_order.order(cappelletti_two_diamonds_responses.MinorEscape, natural_suited_part_scores)
 # We'd rather be invitational to game than just a law bid.
 rule_order.order(natural_suited_part_scores, cappelletti_two_diamonds_invitational_responses)
-
-
-class ResponseToCappellettiTwoDiamonds(ResponseToCappelletti):
-    preconditions = LastBidWas(positions.Partner, '2D')
-    constraints = {
-        'P':  [(diamonds >= 6, ThreeOfTheTopFiveOrBetter(suit.DIAMONDS)), cappelletti_two_diamonds_responses.LongDiamonds],
-        '2N': (NO_CONSTRAINTS, cappelletti_two_diamonds_responses.MinorEscape),
-        '3C':  [(clubs >= 6, ThreeOfTheTopFiveOrBetter(suit.CLUBS)), cappelletti_two_diamonds_responses.LongClubs],
-
-        # Could these be natural too?  They imply invitational points?  But how many does partner have?
-        '3H': [(MinimumCombinedLength(9), MinimumCombinedPoints(22)), cappelletti_two_diamonds_responses.InvitationalHeartSupport], 
-        '3S': [(MinimumCombinedLength(9), MinimumCombinedPoints(22)), cappelletti_two_diamonds_responses.InvitationalSpadeSupport],
-    }
-    annotations_per_call = {
-        '2N': annotations.Artificial,
-    }
 
 
 class ResponseToCappellettiTwoDiamonds(ResponseToCappelletti):
