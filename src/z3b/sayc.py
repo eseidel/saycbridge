e796d7bec5232e59754fe91873e1c42a74edaf03
Eric Seidel <github@eseidel.com>
2013-09-10 23:49:55 -0500

    Assert that our rule collection code is not producing duplicates

--- src/z3b/sayc.py
+++ src/z3b/sayc.py
@@ -15,5 +15,7 @@ def _concrete_rule_classes():
 
 class StandardAmericanYellowCard(object):
     # Rule ordering does not matter.  We could have python crawl the files to generate this list instead.
+    # rules cannot currently be a set() as CompiledRule is not hashable.
     rules = [RuleCompiler.compile(description_class) for description_class in _concrete_rule_classes()]
+    assert len(rules) == len([rule.name for rule in rules]), "Duplicate rules!"
     priority_ordering = rule_order
