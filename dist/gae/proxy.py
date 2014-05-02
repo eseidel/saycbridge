a06faf3e374f283c98b0c1e5454cd085a286c2df
Eric Seidel <github@eseidel.com>
2014-05-02 12:48:58 -0700

    Further simplification to HandConstraints

--- dist/gae/proxy.py
+++ dist/gae/proxy.py
@@ -13,32 +13,15 @@ from z3b.preconditions import annotations
 from z3b.forcing import SAYCForcingOracle
 
 
-# FIXME: This is an over-complicated artifact from the KBB.
-class HandConstraints(object):
+class ConstraintsSerializer(object):
     MAX_HCP_PER_HAND = 37
     EMPTY_HCP_RANGE = (0, MAX_HCP_PER_HAND)
 
-    def __init__(self):
-        self._hcp_range = self.EMPTY_HCP_RANGE
-        self._suit_length_ranges = [(0, 13) for suit in SUITS]
+    def __init__(self, position_view):
+        self._hcp_range = (position_view.min_points, position_view.max_points)
+        self._suit_length_ranges = [(position_view.min_length(suit), position_view.max_length(suit)) for suit in SUITS]
 
-    def _range_from_tuple(self, range_tuple):
-        return range(range_tuple[0], range_tuple[1] + 1)
-
-    def _updated_range_tuple(self, old_tuple, new_min, new_max):
-        old_min, old_max = old_tuple
-        return (max(new_min, old_min), min(new_max, old_max))
-
-    # FIXME: This name is slightly misleading, since we are actually just restricting the range.
-    def set_hcp_range(self, min_hcp, max_hcp):
-        self._hcp_range = self._updated_range_tuple(self._hcp_range, min_hcp, max_hcp)
-
-    def set_length_range(self, suit, min_length, max_length, disable_implied_length_update=None):
-        assert suit in SUITS, "%s is not a suit!" % suit
-        previous_length_range = self._suit_length_ranges[suit.index]
-        self._suit_length_ranges[suit.index] = self._updated_range_tuple(self._suit_length_ranges[suit.index], min_length, max_length)
-
-    def _string_for_range(self, range_tuple, global_max=None):
+    def _string_for_range(self, range_tuple, global_max):
         # This len check only exists for trying to print invalid hand constraints.
         min_value, max_value = range_tuple
         if min_value == max_value:
@@ -47,14 +30,11 @@ class HandConstraints(object):
             return "?"  # To indicate no information.
         if max_value >= global_max:
             return "%s+" % min_value
-        # Could use <5 syntax, but that looks strange for suit lengths.
-        # if min_value == 0:
-        #     return "<%s" % max_value
         return "%s-%s" % (min_value, max_value)
 
     def _pretty_string_for_suit(self, suit, max_suit_length_to_show=None):
         max_suit_length_to_show = max_suit_length_to_show or 6
-        suit_string = self._string_for_range(self._suit_length_ranges[suit.index], global_max=max_suit_length_to_show)
+        suit_string = self._string_for_range(self._suit_length_ranges[suit.index], max_suit_length_to_show)
         if suit_string == "?":
             return None
         return suit_string + suit.char
@@ -66,20 +46,10 @@ class HandConstraints(object):
         suit_strings = [self._pretty_string_for_suit(suit) for suit in SUITS]
         # Don't bother to show suits we know nothing about.
         suit_strings = filter(lambda string: bool(string), suit_strings)
-        pretty_string = "%s hcp" % self._string_for_range(self._hcp_range, global_max=self.MAX_HCP_PER_HAND)
+        pretty_string = "%s hcp" % self._string_for_range(self._hcp_range, self.MAX_HCP_PER_HAND)
         if suit_strings:
             return "%s, %s" % (pretty_string, " ".join(suit_strings))
         return pretty_string
-
-
-# FIXME: This is a horrible hack.  Callers should move off of the KBB HandConstraints API.
-def _position_knowledge_from_position_view(position_view):
-    kbb_constraints = HandConstraints()
-    kbb_constraints.set_hcp_range(position_view.min_points, position_view.max_points)
-    for suit in SUITS:
-        kbb_constraints.set_length_range(suit, position_view.min_length(suit), position_view.max_length(suit))
-    # FIXME: What about honor concentration?
-    return kbb_constraints
 
 
 # FIXME: This is a hack to make the GAE front-end work with the Z3 Bidder.
@@ -96,7 +66,7 @@ class CallSelectionProxy(object):
 
         try:
             with Interpreter().extend_history(self.call_selection.rule_selector.history, self.call) as history:
-                return _position_knowledge_from_position_view(history.rho)
+                return ConstraintsSerializer(history.rho)
         except InconsistentHistoryException:
             return None
 
@@ -118,13 +88,11 @@ class InterpreterProxy(object):
         self.interpreter = Interpreter()
 
     def _pretty_string_for_position_view(self, position_view):
-        # kbb HandConstraints just so we can use it's pretty_print function.
-        position_knowledge = _position_knowledge_from_position_view(position_view)
-        kbb_oneline = position_knowledge.explore_string()
+        explore_string = ConstraintsSerializer(position_view).explore_string()
         # FIXME: Annotation filtering belongs on the client, not here!
         annotations_whitelist = set([annotations.Artificial, annotations.NotrumpSystemsOn])
         annotations_for_last_call = set(position_view.annotations_for_last_call) & annotations_whitelist
-        pretty_string = "%s %s" % (kbb_oneline, ", ".join(map(str, annotations_for_last_call)))
+        pretty_string = "%s %s" % (explore_string, ", ".join(map(str, annotations_for_last_call)))
         # Only bother trying to interpret if the bid is forcing if we understood it in the first place:
         if position_view.rule_for_last_call:
             try:
