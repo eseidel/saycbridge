876eac9bc7da54d846c5ec52ac0498eb37c9ef0a
Eric Seidel <github@eseidel.com>
2019-04-25 21:48:06 -0700

    Upgrade to Flutter  1.4.9-hotfix.1 (#40)
    * First attempt to update to latest flutter
    
    * First attempt to update to latest flutter
    
    * Update to Flutter 1.4.9-hotfix.1
    
    * Update template (non-dart files) to Flutter 1.4.9-hotfix.1

--- dist/flx/lib/interpreter.dart
+++ dist/flx/lib/interpreter.dart
@@ -5,14 +5,14 @@
 import 'dart:async';
 import 'dart:convert';
 
-import 'package:flutter/http.dart' as http;
+import 'package:http/http.dart' as http;
 
 import 'model.dart';
 
 String _getUrl({
   String callString: '',
   String dealer: 'N',
-  String vulnerability: 'None'
+  String vulnerability: 'None',
 }) {
   return 'https://sayc.abortz.net/json/interpret2?calls_string=${Uri.encodeQueryComponent(callString)}&dealer=${Uri.encodeQueryComponent(dealer)}&vulnerability=${Uri.encodeQueryComponent(vulnerability)}';
 }
@@ -24,11 +24,11 @@ Future<List<CallInterpretation>> getInterpretations(CallHistory callHistory) {
   String url = _getUrl(callString: callHistory.calls.join(','));
   return _memoryCache.putIfAbsent(url, () async {
     try {
-      return JSON.decode(await http.read(url)).map((item) {
+      return json.decode(await http.read(url)).map<CallInterpretation>((item) {
         return new CallInterpretation(
           ruleName: item['rule_name'],
           knowledge: item['knowledge_string'],
-          call: new Call.fromName(item['call_name'])
+          call: new Call.fromName(item['call_name']),
         );
       }).toList();
     } catch (e) {
--- dist/flx/lib/model.dart
+++ dist/flx/lib/model.dart
@@ -2,13 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-enum Strain {
-  clubs,
-  diamonds,
-  hearts,
-  spades,
-  notrump
-}
+enum Strain { clubs, diamonds, hearts, spades, notrump }
 
 String getStringForStrain(Strain strain) {
   return const {
@@ -21,16 +15,11 @@ String getStringForStrain(Strain strain) {
 }
 
 Strain getStrainFromString(String name) {
-  if (name == 'C')
-    return Strain.clubs;
-  if (name == 'D')
-    return Strain.diamonds;
-  if (name == 'H')
-    return Strain.hearts;
-  if (name == 'S')
-    return Strain.spades;
-  if (name == 'N')
-    return Strain.notrump;
+  if (name == 'C') return Strain.clubs;
+  if (name == 'D') return Strain.diamonds;
+  if (name == 'H') return Strain.hearts;
+  if (name == 'S') return Strain.spades;
+  if (name == 'N') return Strain.notrump;
   return null;
 }
 
@@ -38,18 +27,14 @@ final int _runeC = 'C'.codeUnitAt(0);
 final int _runeD = 'D'.codeUnitAt(0);
 final int _runeH = 'H'.codeUnitAt(0);
 final int _runeS = 'S'.codeUnitAt(0);
+final int _runeN = 'N'.codeUnitAt(0);
 
 Strain getStrainFromRune(int rune) {
-  if (rune == _runeC)
-    return Strain.clubs;
-  if (rune == _runeD)
-    return Strain.diamonds;
-  if (rune == _runeH)
-    return Strain.hearts;
-  if (rune == _runeS)
-    return Strain.spades;
-  if (rune == 'N')
-    return Strain.notrump;
+  if (rune == _runeC) return Strain.clubs;
+  if (rune == _runeD) return Strain.diamonds;
+  if (rune == _runeH) return Strain.hearts;
+  if (rune == _runeS) return Strain.spades;
+  if (rune == _runeN) return Strain.notrump;
   return null;
 }
 
@@ -63,11 +48,7 @@ String getSymbolForStrain(Strain strain) {
   }[strain];
 }
 
-enum SpecialCall {
-  pass,
-  double,
-  redouble
-}
+enum SpecialCall { pass, double, redouble }
 
 String getStringSpecialCall(SpecialCall specialCall) {
   return const {
@@ -83,15 +64,14 @@ class Call {
     assert(strain != null);
   }
 
-  const Call.special(this.specialCall) : level = null, strain = null;
+  const Call.special(this.specialCall)
+      : level = null,
+        strain = null;
 
   factory Call.fromName(String callName) {
-    if (callName == 'P')
-      return pass;
-    if (callName == 'X')
-      return x;
-    if (callName == 'XX')
-      return xx;
+    if (callName == 'P') return pass;
+    if (callName == 'X') return x;
+    if (callName == 'XX') return xx;
     return new Call(int.parse(callName[0]), getStrainFromString(callName[1]));
   }
 
@@ -110,24 +90,15 @@ class Call {
   static const Call xx = const Call.special(SpecialCall.redouble);
 
   String toString() {
-    if (isContract)
-      return '$level${getStringForStrain(strain)}';
+    if (isContract) return '$level${getStringForStrain(strain)}';
     return getStringSpecialCall(specialCall);
   }
 }
 
-enum Positions {
-  west,
-  north,
-  east,
-  south
-}
+enum Positions { west, north, east, south }
 
 class CallHistory {
-  CallHistory({
-    this.calls: const [],
-    this.dealer: Positions.north
-  });
+  CallHistory({this.calls: const [], this.dealer: Positions.north});
 
   final List<Call> calls;
   final Positions dealer;
@@ -135,66 +106,57 @@ class CallHistory {
   CallHistory extendWithCall(Call call) {
     return new CallHistory(
       calls: new List<Call>.from(calls)..add(call),
-      dealer: dealer
+      dealer: dealer,
     );
   }
 
   bool get isComplete {
-    if (calls.length < 4)
-      return false;
+    if (calls.length < 4) return false;
     for (int i = calls.length - 3; i < calls.length; ++i) {
-      if (!calls[i].isPass)
-        return false;
+      if (!calls[i].isPass) return false;
     }
     return true;
   }
 
   int get lastNonPassIndex {
     for (int i = calls.length - 1; i >= 0; --i) {
-      if (!calls[i].isPass)
-        return i;
+      if (!calls[i].isPass) return i;
     }
     return null;
   }
 
   Positions getPositionForIndex(int index) {
-    return Positions.values[
-      (dealer.index + index) % Positions.values.length
-    ];
+    return Positions.values[(dealer.index + index) % Positions.values.length];
   }
 
   Call get lastContract {
     for (Call call in calls.reversed) {
-      if (call.isContract)
-        return call;
+      if (call.isContract) return call;
     }
     return null;
   }
 
   Iterable<Call> get possibleCalls sync* {
-    if (isComplete)
-      return;
+    if (isComplete) return;
 
     yield Call.pass;
 
     int lastNonPassIndex = this.lastNonPassIndex;
-    if (lastNonPassIndex == calls.length - 1 || lastNonPassIndex == calls.length - 3) {
+    if (lastNonPassIndex == calls.length - 1 ||
+        lastNonPassIndex == calls.length - 3) {
       Call lastNonPass = calls[lastNonPassIndex];
       if (lastNonPass.isContract)
         yield Call.x;
-      else if (lastNonPass.isDouble)
-        yield Call.xx;
+      else if (lastNonPass.isDouble) yield Call.xx;
     }
 
     Call lastContract = this.lastContract;
     for (int level = 1; level < 8; ++level) {
-      if (lastContract != null && level < lastContract.level)
-        continue;
+      if (lastContract != null && level < lastContract.level) continue;
       for (Strain strain in Strain.values) {
         if (lastContract != null &&
             level == lastContract.level &&
-            strain.index <= lastContract.strain.index)
-          continue;
+            strain.index <= lastContract.strain.index) continue;
         yield new Call(level, strain);
       }
     }
@@ -216,7 +178,7 @@ class CallInterpretation {
     this.ruleName,
     this.knowledge,
     this.call,
-    this.isTentative: false
+    this.isTentative: false,
   });
 
   final String ruleName;
